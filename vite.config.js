a0784a780791964e92303c15ef9c74148d5c1d16
rahularya002 <115874802+rahularya002@users.noreply.github.com>
2024-07-22 12:03:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-intersection-observer': require.resolve('react-intersection-observer')
-    }
+  build: {
+    rollupOptions: {
+      external: ['react-intersection-observer'],
+    },
   },
 });
